56e23fe0edc5140b669d090f9a4099f9a2ff51db
Jose1697 <josenetboy@hotmail.com>
2021-04-09 21:33:08 -0500

    7. Firmando y verificando nuestro jwt

---
+++ index.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+
+const [ , ,option, secret, nameOrtoken] = process.argv; //los argumentos lo vamos a sacar del terminal, 
+//los 2 primeros parametros q no se definen son el proceso de node y el archivo que estamos leyendo
+//option = verificar o firmar
+
+if( !option || !secret || !nameOrtoken){
+    return console.log('Missing arguments');
+}
+
+function signToken(payload, secret){ //Firmar JWT
+    return jwt.sign(payload, secret);
+}
+
+function verifyToken(token, secret) { //Verificar JWT
+    return jwt.verify(token,secret) //retorna el token decodificado
+}
+
+if(option === 'sign'){
+    console.log(signToken({ sub: nameOrtoken }, secret));
+}else if(option === 'verify'){
+    console.log(verifyToken(nameOrtoken, secret));
+} else {
+    console.log('Option needs to be "sign" or "verify" ');
+}
